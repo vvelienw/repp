332a6a39069d24c17babb71d03e04ce09daca8c7
vvelienw <151198042+vvelienw@users.noreply.github.com>
2024-04-16 20:31:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+main = Flask(__name__)
+main.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+main.config['SCLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(main)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@main.route('/')
+@main.route('/home')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@main.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template('create-article.html')
+
+
+if __name__ == "__main__":
+    main.run(debug=True)
